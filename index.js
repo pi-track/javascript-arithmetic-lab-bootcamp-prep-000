f80f2fb054f3da55265598b31a08a1ee84b529c8
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-01 21:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,14 +33,7 @@ function divide(a,b){
 function makeInt(n){
   return parseInt(n,10)
 }
-/*
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
-*/
+function preserveDecimal(n){
+  return parseFloat(n)
+}
