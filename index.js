bb77068897a52ea1c703cd436fbd10197f201747
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-01 21:15:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b){
+  return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function increment(n){
+  return n++
+}
+
+function decrement(n){
+  return n--
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function makeInt(n){
+  return parseInt(n,10)
+}
+/*
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+*/
